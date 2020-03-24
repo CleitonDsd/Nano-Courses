19262208606f4af632d346ad5a638bec68043327
Cleiton Dias <s.diascleiton@gmail.com>
2020-03-23 21:19:48 -0300

    A1 - first project

---
+++ HelloJava/src/com/java/HelloJava.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import javax.swing.JOptionPane;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		JOptionPane.showMessageDialog(null, "Olá, mundo");
+	}
+
+}
